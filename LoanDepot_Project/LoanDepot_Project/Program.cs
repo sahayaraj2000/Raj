85120e021a549559ccdb5ee97e207d94613147b5
sahayaraj2000 <46437940+sahayaraj2000@users.noreply.github.com>
2019-01-07 20:35:13 -0800

    Project for LoanDepot

---
+++ LoanDepot_Project/LoanDepot_Project/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanDepot_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 1000;
+            int result = GetSumOfMultiples(value);
+            Console.WriteLine("The Sum of all the Multiples of 3 or 5 below {0} is {1}", value, result);
+            Console.Read();
+        }
+        static int GetSumOfMultiples(int value)
+        {
+            int i, j, k;
+            int result = 0;
+            for (i = 1; i < value; i++)
+            {
+                j = i % 3;
+                k = i % 5;
+                if (j == 0 || k == 0)
+                {
+                    result = result + i;
+                }
+            }
+            return result;
+        }
+    }
+}
